4f0477318ff44e16157f9e0ec985d80d55170caa
Vijaykumar-2005 <vvijakk@gmail.com>
2026-06-30 11:55:11 +0530

    Added Day1 2nd problem

---
+++ Day1/REMOVE_DUPLICATES/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package TWO_SUM;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+public class RemoveDuplicates{
+    public static void main(String[] args) {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        System.out.println("Enter n:");
+        int n = Integer.parseInt(br.readLine());
+        System.out.println("Enter n number of Elements:");
+        String[] s = br.readLine().split(" ");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++) {
+            a[i] = Integer.parseInt(s[i]);
+        }
+        int i = 0;
+        for(int j = 0;j < n;j++) {
+            if(a[i] != a[j]) {
+                a[++i] = a[j];
+            }
+        }
+        System.out.print("[ ");
+        for(int j=0;j<i+!;j++) {
+            System.out.print(a[j]+" ");
+        }
+        System.out.print("]");
+    }
+}
