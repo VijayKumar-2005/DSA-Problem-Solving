19481de2c9f6ef98464743eb1b11ad66f059b1d1
VijayKumar-2005 <vvijakk@gmail.com>
2026-06-30 13:01:36 +0530

    Added Day1 problem3

---
+++ Day1/REMOVE_ELEMENT/RemoveElement.java
@@ -0,0 +1,30 @@
+package REMOVE_ELEMENT;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+public class RemoveElement {
+    public static void main(String[] args) throws Exception{
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        System.out.println("Enter n:");
+        int n = Integer.parseInt(br.readLine());
+        System.out.println("Enter n number of Elements:");
+        String[] s = br.readLine().split(" ");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++) {
+            a[i] = Integer.parseInt(s[i]);
+        }
+        System.out.println("Enter k:");
+        int k = Integer.parseInt(br.readLine());
+        int i = -1;
+        for(int j=0;j<n;j++) {
+            if(a[j] != k) {
+                a[++i] = a[j];
+            }
+        }
+        System.out.print("[ ");
+        for(int j=0;j<i+1;j++) {
+            System.out.print(a[j]+" ");
+        }
+        System.out.print("]");
+    }
+}
